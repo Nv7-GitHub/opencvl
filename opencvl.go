1223d2d8af57072b5e80f7732cd76a3c878cc8fc
Nv7 <junk4Nv7@gmail.com>
2021-01-20 11:42:31 -0800

    Added basic pipeline functionality

--- opencvl.go
+++ opencvl.go
@@ -1,11 +1,13 @@
 package opencvl
 
-import "image"
+import (
+	"errors"
+	"image"
+)
 
 // Pipeline contains all necessary data for a opencvl pipeline
 type Pipeline struct {
-	device   int
-	platform int
+	layers []Layer
 }
 
 // Layer is a layer of an OpenCVL pipeline
@@ -14,4 +16,68 @@ type Layer interface {
 	execute(img *image.RGBA) (*image.RGBA, error)
 	setArgs([]interface{}) error
 	build() error
+}
+
+// NewPipeline creates a new Pipeline
+func NewPipeline() Pipeline {
+	return Pipeline{
+		layers: make([]Layer, 0),
+	}
+}
+
+// AddLayer adds a layer to a pipeline
+func (p *Pipeline) AddLayer(layer Layer) error {
+	kind := layer.Type()
+	if kind != "opencv" && kind != "opencl" {
+		return errors.New("Invalid pipeline kind")
+	}
+	p.layers = append(p.layers, layer)
+	return nil
+}
+
+// SetOpenCVArgs sets the arguments for all OpenCV layers
+func (p *Pipeline) SetOpenCVArgs(args ...interface{}) {
+	for _, layer := range p.layers {
+		if layer.Type() == "opencv" {
+			layer.setArgs(args)
+		}
+	}
+}
+
+// SetOpenCLArgs sets the arguments for all OpenCL layers
+func (p *Pipeline) SetOpenCLArgs(args ...interface{}) error {
+	var err error
+	for _, layer := range p.layers {
+		if layer.Type() == "opencl" {
+			err = layer.setArgs(args)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Build compiles the pipeline
+func (p *Pipeline) Build() error {
+	var err error
+	for _, layer := range p.layers {
+		err = layer.build()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ExecuteOnImage executes the pipeline on an image
+func (p *Pipeline) ExecuteOnImage(img *image.RGBA) (*image.RGBA, error) {
+	var err error
+	for _, layer := range p.layers {
+		img, err = layer.execute(img)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return img, nil
 }

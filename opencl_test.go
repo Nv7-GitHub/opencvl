305e0c7a9a753163f205224de4520034c5eb1fb8
Nv7 <junk4Nv7@gmail.com>
2021-01-20 18:08:52 -0800

    OpenCL video example too

--- opencl_test.go
+++ opencl_test.go
@@ -71,6 +71,44 @@ func TestOpenCLLayer(t *testing.T) {
 	}
 }
 
+func TestOpenCLLayer_Video(t *testing.T) {
+	var kernelSource = `
+	__kernel void invert(
+		__read_only image2d_t in,
+		__write_only image2d_t out)
+	{
+		const int2 pos = (int2)(get_global_id(0), get_global_id(1));
+		const int2 dim = get_image_dim(in);
+		float4 pixel = (float4)(0);
+		if (pos.x < dim.x && pos.y < dim.y) {
+			pixel = read_imagef(in, pos);
+			pixel = (float4)(1) - pixel;
+			pixel.w = 1;
+			write_imagef(out, pos, pixel);
+		}
+	}`
+
+	pipeline := NewPipeline()
+	layer, err := NewOpenCLLayer(kernelSource, "invert", 0, 0)
+	if err != nil {
+		panic(err)
+	}
+	err = pipeline.AddLayer(layer)
+	if err != nil {
+		panic(err)
+	}
+
+	err = pipeline.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	err = pipeline.ExecuteOnVideo("sample.mp4", "outcl.mp4")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func BenchmarkOpenCLLayer(b *testing.B) {
 	file, err := os.Open("gopher.png")
 	if err != nil {
